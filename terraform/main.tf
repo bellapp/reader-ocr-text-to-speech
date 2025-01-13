e47020b56b48fa250209dced94ea24ee00d54ba5
Abdelaaziz Bellout <bellout@gmail.com>
2025-01-13 12:23:41 +0100

    add iam role with ECS

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,10 +31,12 @@ resource "aws_ecs_cluster" "main" {
 # ECS Task Definition
 resource "aws_ecs_task_definition" "app" {
   family                   = var.app_name
-  network_mode            = "awsvpc"
+  network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                     = 256
-  memory                  = 512
+  cpu                      = 256
+  memory                   = 512
+
+  execution_role_arn = aws_iam_role.ecs_task_execution.arn
 
   container_definitions = jsonencode([
     {
@@ -137,4 +139,30 @@ resource "aws_lb_target_group" "app" {
   protocol = "HTTP"
   vpc_id   = module.vpc.vpc_id
   target_type = "ip"
+}
+
+resource "aws_iam_role" "ecs_task_execution" {
+  name = "${var.app_name}-ecs-task-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+resource "aws_cloudwatch_log_group" "ecs" {
+  name              = "/ecs/${var.app_name}"
+  retention_in_days = 7
 }
